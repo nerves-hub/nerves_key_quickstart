55592e6211591640c8ab36b86cfe10fb2764aa64
Frank Hunleth <fhunleth@troodon-software.com>
2019-09-12 10:41:29 -0400

    Don't strip beams for dev builds

--- nerves_key_quickstart_fw/mix.exs
+++ nerves_key_quickstart_fw/mix.exs
@@ -79,7 +79,8 @@ defmodule NervesKeyQuickstartFw.MixProject do
       overwrite: true,
       cookie: "#{@app}_cookie",
       include_erts: &Nerves.Release.erts/0,
-      steps: [&Nerves.Release.init/1, :assemble]
+      steps: [&Nerves.Release.init/1, :assemble],
+      strip_beams: Mix.env() == :prod
     ]
   end
 end
