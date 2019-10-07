8cff1130acecfe717f84b8a860989fb72025c6ff
Justin Schneck <jschneck@mac.com>
2019-10-07 15:22:28 -0400

    v0.2.1 release

--- nerves_key_quickstart_fw/mix.exs
+++ nerves_key_quickstart_fw/mix.exs
@@ -2,7 +2,7 @@ defmodule NervesKeyQuickstartFw.MixProject do
   use Mix.Project
 
   @app :nerves_key_quickstart_fw
-  @version "0.2.0"
+  @version "0.2.1"
   @all_targets [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a, :rpi4, :bbb, :x86_64]
 
   def project do
